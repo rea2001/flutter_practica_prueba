3172238c92db803d1f04eac927ac05d6bb454180
rea2001 <reaalexandra11@gmail.com>
2024-04-16 17:41:29 -0500

    Creacion de clases

---
+++ lib/clase2.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class clase2  extends StatelessWidget {
+  const clase2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/prueba.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class prueba  extends StatefulWidget {
+  const prueba({super.key});
+
+  @override
+  State<prueba> createState() => _pruebaState();
+}
+
+class _pruebaState extends State<prueba> {
+  @override
+  Widget build(BuildContext context) {
+    print('Hola gato');
+    return MaterialApp(
+       
+    );
+  }
+}
